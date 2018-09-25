4936deb86ca3fd63f1c7b5605891d553c8632e87
delphikettle <delphikettle@gmail.com>
2018-09-25 14:38:35 +0300

    added number of calcs

--- lab1/main.py
+++ lab1/main.py
@@ -8,7 +8,7 @@ def dihiotomia(f, a, b, eps):
     it_no = 0
 
     while b - a >= eps:
-        it_no += 1
+        it_no += 2
 
         delta = eps / 2.25
         x0 = (b + a) / 2 - delta
@@ -19,31 +19,42 @@ def dihiotomia(f, a, b, eps):
         else:
             a = x0
 
-    return (b + a) / 2
+    return ((b + a) / 2, it_no)
 
 
 phi = (1 + sqrt(5)) / 2
 
 
 def golden_section(f, a, b, eps):
+    it_no = 0
+    x1 = a
+    x2 = b
+    f1, f2 = f(x1), f(x2)
     while b - a > eps:
         t = (b - a) / phi
-        x0 = b - t
-        x1 = a + t
-
-        if f(x0) < f(x1):
-            b = x1
+        if f1 < f2:
+            a = x1
+            x1 = x2
+            f1 = f2
+            x2 = a + t
+            f2 = f(x2)
         else:
-            a = x0
+            b = x2
+            x2 = x1
+            f2 = f1
+            x1 = b - t
+            f1 = f(x1)
+        it_no += 1
 
-    return (b + a) / 2
+    return ((b + a) / 2, it_no)
 
 
 def meth_fibo(f, a, b, eps):
-    a0, b0 = a, b
+    it_no = 0
     # get n
-    n = 0
-    while (b0 - a0) / eps >= fibo(n + 2):
+    n = 1
+
+    while abs(b - a) / eps < fibo(n):
         n += 1
 
     k = 0
@@ -66,8 +77,9 @@ def meth_fibo(f, a, b, eps):
             f1 = f(x1)
 
         k += 1
+        it_no += 1
 
-    return x1
+    return (x1, it_no)
 
 
 def linear_min(f, eps):
@@ -87,12 +99,11 @@ def linear_min(f, eps):
         prev, x0, x1 = x0, x1, x0 + h
         f0, f1 = f1, f(x1)
 
-    return meth_fibo(f, prev, x1, eps)
-
+    return meth_fibo(f, prev, x1, eps)[0]
 
 
 if __name__ == '__main__':
-    eps = 0.00000000000001
+    eps = 0.00001
 
     print(f'Метод дихотомии: x = {dihiotomia(f, a, b, eps)}')
     print(f'Метод золотого сечения: x = {golden_section(f, a, b, eps)}')
